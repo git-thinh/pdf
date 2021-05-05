75d0e8307490d6187e151d6fde2e50f4542ba9c4
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2021-05-05 15:28:57 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Linq;
+using Newtonsoft.Json;
+using System.IO;
+using PdfiumViewer;
+using System.Drawing.Imaging;
+
+class Program
+{
+    const int __PORT_WRITE = 1000;
+    const int __PORT_READ = 1001;
+    const string __SUBCRIBE_IN = "__PDF_IN";
+    const string __SUBCRIBE_OUT = "__PDF_OUT";
+    static RedisBase m_subcriber;
+    static bool __running = true;
+
+    static byte[] _pageAsBitmapBytes(PdfDocument doc, int pageCurrent)
+    {
+        int w = (int)doc.PageSizes[pageCurrent].Width;
+        int h = (int)doc.PageSizes[pageCurrent].Height;
+
+        ////if (w >= h) w = this.Width;
+        ////else w = 1200;
+        //if (w < 1200) w = 1200;
+        //h = (int)((w * doc.PageSizes[i].Height) / doc.PageSizes[i].Width);
+
+        if (w > 1200)
+        {
+            w = 1200;
+            h = (int)((w * doc.PageSizes[pageCurrent].Height) / doc.PageSizes[pageCurrent].Width);
+        }
+
+        using (var image = doc.RenderTransparentBG(pageCurrent, w, h, 100, 100))
+        using (var ms = new MemoryStream())
+        {
+            image.Save(ms, ImageFormat.Png);
+            return ms.ToArray();
+        }
+    }
+
+    static void _updateDocInfo(string requestId, string file, PDF_STORE store)
+    {
+        long docInfoId = 0;
+        if (File.Exists(file))
+        {
+            var redis = new RedisBase(new RedisSetting(REDIS_TYPE.ONLY_WRITE, __PORT_WRITE));
+            var cmd = PDF_COMMAND.GET_DOC_INFO;
+            try
+            {
+                using (var doc = PdfDocument.Load(file))
+                {
+                    int pageTotal = doc.PageCount;
+                    long fileSize = new FileInfo(file).Length;
+                    //docId = StaticDocument.BuildId(DOC_TYPE.PNG_OGRINAL, pageTotal, fileSize);
+                    docInfoId = StaticDocument.BuildId(DOC_TYPE.INFO_OGRINAL, pageTotal, fileSize);
+                    var docInfo = new oDocument()
+                    {
+                        id = docInfoId,
+                        file_length = fileSize,
+                        file_name_ascii = "",
+                        file_name_ogrinal = Path.GetFileNameWithoutExtension(file),
+                        file_page = pageTotal,
+                        file_path = file,
+                        file_type = DOC_TYPE.PDF_OGRINAL,
+                        infos = doc.GetInformation().toDictionary(),
+                        metadata = ""
+                    };
+                    var json = JsonConvert.SerializeObject(docInfo, Formatting.Indented);
+                    var bsInfo = Encoding.UTF8.GetBytes(json);
+                    var lz = LZ4.LZ4Codec.Wrap(bsInfo);
+                    ////var decompressed = LZ4Codec.Unwrap(compressed);
+                    redis.HSET("_DOC_INFO", docInfoId.ToString(), lz);
+                }
+                if (docInfoId > 0)
+                    redis.PUBLISH(__SUBCRIBE_OUT, string.Format("{0}{1}", requestId, docInfoId));
+            }
+            catch (Exception exInfo)
+            {
+                string errInfo = cmd.ToString() + " -> " + file + Environment.NewLine + exInfo.Message + Environment.NewLine + exInfo.StackTrace;
+                redis.HSET("_ERROR:PDF:" + cmd.ToString(), docInfoId.ToString(), errInfo);
+            }
+        }
+    }
+
+    static void _splitAllJpeg(string requestId, string file, PDF_STORE store)
+    {
+    }
+
+    static void _splitAllPng(string requestId, string file, PDF_STORE store)
+    {
+        if (File.Exists(file))
+        {
+            var redis = new RedisBase(new RedisSetting(REDIS_TYPE.ONLY_WRITE, __PORT_WRITE));
+            var cmd = PDF_COMMAND.SPLIT_ALL_PNG;
+            long docId = 0;
+            try
+            {
+                using (var doc = PdfDocument.Load(file))
+                {
+                    int pageTotal = doc.PageCount;
+                    docId = StaticDocument.BuildId(DOC_TYPE.PNG_OGRINAL, pageTotal, new FileInfo(file).Length);
+                    var sizes = new Dictionary<string, string>();
+                    for (int i = 0; i < pageTotal; i++)
+                    {
+                        byte[] buf = null;
+                        string slen = "";
+                        bool ok = false;
+                        string err = "";
+                        try
+                        {
+                            buf = _pageAsBitmapBytes(doc, i);
+                            slen = buf.Length.ToString();
+                            ok = redis.HSET(docId, i, buf);
+                        }
+                        catch (Exception ex)
+                        {
+                            err = ex.Message + Environment.NewLine + ex.StackTrace;
+                        }
+                        redis.ReplyStatus(__SUBCRIBE_OUT, requestId, cmd.ToString(), ok ? 1 : 0, docId, i, file, err);
+                        sizes.Add(string.Format("{0}:{1}", docId, i), slen);
+                    }
+                    redis.HMSET("_IMG_SIZE", sizes);
+                    redis.ReplyStatus(__SUBCRIBE_OUT, requestId, cmd.ToString(), 1, docId);
+                }
+            }
+            catch (Exception exInfo)
+            {
+                string errInfo = cmd.ToString() + " -> " + file + Environment.NewLine + exInfo.Message + Environment.NewLine + exInfo.StackTrace;
+                redis.HSET("_ERROR:PDF:" + cmd.ToString(), docId.ToString(), errInfo);
+            }
+        }
+    }
+
+    static void __executeBackground(byte[] buf)
+    {
+        if (buf.Length < 39) return;
+        string requestId = Encoding.ASCII.GetString(buf, 0, 36);
+        var cmd = (PDF_COMMAND)((int)buf[36]);
+        var store = (PDF_STORE)((int)buf[37]);
+        string file = Encoding.UTF8.GetString(buf, 38, buf.Length - 38);
+        switch (cmd)
+        {
+            case PDF_COMMAND.GET_DOC_INFO:
+                _updateDocInfo(requestId, file, store);
+                break;
+            case PDF_COMMAND.SPLIT_ALL_JPG:
+                _splitAllJpeg(requestId, file, store);
+                break;
+            case PDF_COMMAND.SPLIT_ALL_PNG:
+                _splitAllPng(requestId, file, store);
+                break;
+        }
+    }
+
+    #region [ SETUP WINDOWS SERVICE ]
+
+    static void __startApp()
+    {
+        m_subcriber = new RedisBase(new RedisSetting(REDIS_TYPE.ONLY_SUBCRIBE, __PORT_READ));
+        m_subcriber.PSUBSCRIBE(__SUBCRIBE_IN);
+        var bs = new List<byte>();
+        while (__running)
+        {
+            if (!m_subcriber.m_stream.DataAvailable)
+            {
+                if (bs.Count > 0)
+                {
+                    var buf = m_subcriber.__getBodyPublish(__SUBCRIBE_IN, bs.ToArray());
+                    bs.Clear();
+                    new Thread(new ParameterizedThreadStart((o) => __executeBackground((byte[])o))).Start(buf);
+                }
+                Thread.Sleep(100);
+                continue;
+            }
+            byte b = (byte)m_subcriber.m_stream.ReadByte();
+            bs.Add(b);
+        }
+    }
+
+    static void __stopApp() => __running = false;
+
+    // FOR SETTING OF WINDOWS SERVICE
+
+    static Thread __threadWS = null;
+    static void Main(string[] args)
+    {
+        if (Environment.UserInteractive)
+        {
+            StartOnConsoleApp(args);
+            Console.WriteLine("Press any key to stop...");
+            Console.ReadKey(true);
+            Stop();
+        }
+        else using (var service = new MyService())
+                ServiceBase.Run(service);
+    }
+
+    public static void StartOnConsoleApp(string[] args) => __startApp();
+    public static void StartOnWindowService(string[] args)
+    {
+        __threadWS = new Thread(new ThreadStart(() => __startApp()));
+        __threadWS.IsBackground = true;
+        __threadWS.Start();
+    }
+
+    public static void Stop()
+    {
+        __stopApp();
+        if (__threadWS != null) __threadWS.Abort();
+    }
+
+    #endregion;
+}
+
